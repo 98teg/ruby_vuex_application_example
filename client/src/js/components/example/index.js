aa8fe98ad20676654c7a8c00c7eb04e929c7e45f
Tomás <tespejo@nosolosoftware.es>
2019-07-15 13:58:09 +0200

    Añadidos los filtros

--- client/src/js/components/example/index.js
+++ client/src/js/components/example/index.js
@@ -4,24 +4,49 @@ export default Vue.extend({
   template: template(),
   data() {
     return {
+      filterTitle: '',
+      filterContent: '',
+      filterUserID: '',
+      filterSince: '',
+      filterUntil: '',
+      lastSort: {},
       posts: []
     };
   },
   methods: {
-    async orderByTitle() {
-      const {data} = await API.posts.index({}, {params: {sort: 'title'}});
+    getFilters(){
+      var filter = {};
+
+      if (this.filterTitle != '') filter = Object.assign({}, filter, {title: this.filterTitle});
+      if (this.filterContent != '') filter = Object.assign({}, filter, {content: this.filterContent});
+      if (this.filterUserID != '') filter = Object.assign({}, filter, {user_id: this.filterUserID});
+      if (this.filterSince != '') filter = Object.assign({}, filter, {since: this.filterSince});
+      if (this.filterUntil != '') filter = Object.assign({}, filter, {until: this.filterUntil});
+
+      return {filter: filter};
+    },
+    getParams(params){
+      return Object.assign({}, this.getFilters(), params)
+    },
+    async getPosts(){
+      const {data} = await API.posts.index({}, {params: this.getParams(this.lastSort)});
     
       this.posts = data;
+    },
+    async orderByTitle() {
+      this.lastSort = {sort: 'title'};
+
+      this.getPosts();
     },
     async orderByDate() {
-      const {data} = await API.posts.index({}, {params: {sort: '-created_at'}});
-    
-      this.posts = data;
+      this.lastSort = {sort: '-created_at'};
+
+      this.getPosts();
     },
     async orderByAuthor() {
-      const {data} = await API.posts.index({}, {params: {sort: 'user_name'}});
-    
-      this.posts = data;
+      this.lastSort = {sort: 'user_name'};
+
+      this.getPosts();
     }
   },
   async created() {
